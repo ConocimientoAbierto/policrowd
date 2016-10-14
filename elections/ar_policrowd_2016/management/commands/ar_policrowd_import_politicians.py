ff60ec2fd49e759cfa0b75e742065284aa28f162
Emanuel Arias <arias.emanuel@gmail.com>
2016-10-14 14:55:51 -0300

    la importación tiene en cuenta los datos previos ejecuta un actualizar o crear según necesite

--- elections/ar_policrowd_2016/management/commands/ar_policrowd_import_politicians.py
+++ elections/ar_policrowd_2016/management/commands/ar_policrowd_import_politicians.py
@@ -15,127 +15,130 @@ from candidates.models.popolo_extra import OrganizationExtra, PersonExtra
 class Command(BaseCommand):
     help = "Imports politicians from 'estructura-organica.csv' to DB (popolo_organization)"
 
+    #contadores
+    areas, organizations, persons, posts, memberships = 0, 0, 0, 0, 0
+
     #argentinaCache --> <Area>
     argentinaCache = None
     def prepareArgentinaCache(self):
-        self.argentinaCache = Area.objects.get_or_create(name='Argentina')[0]
+        area = Area.objects.get_or_create(name='Argentina')
+        if area[1]:
+            self.areas = self.areas + 1
+        self.argentinaCache = area[0]
 
     #executivePowerCache --> <Organization>
     executivePowerCache = None
     def prepareExecutivePowerCache(self):
-        date = time.strftime('%Y-%m-%d %H:%M:%S')
-        self.executivePowerCache = Organization.objects.get_or_create(name='Poder Ejecutivo', area_id=self.argentinaCache.id, defaults={
-            'created_at': date,
-            'updated_at': date,
-            'area_id': self.argentinaCache.id,
-            'classification': 'poder'
-        })[0]
-        Organization.objects.get_or_create(name='Poder Legislativo', area_id=self.argentinaCache.id, defaults={
-            'created_at': date,
-            'updated_at': date,
-            'area_id': self.argentinaCache.id,
-            'classification': 'poder'
-        })
-        Organization.objects.get_or_create(name='Poder Judicial', area_id=self.argentinaCache.id,defaults={
-            'created_at': date,
-            'updated_at': date,
-            'area_id': self.argentinaCache.id,
-            'classification': 'poder'
-        })
+        executivePower = Organization.objects.get_or_create(name= 'Poder Ejecutivo', area_id= self.argentinaCache.id,
+            defaults={
+                'classification': 'poder'
+            }
+        )
+
+        if executivePower[1]:
+            self.organizations = self.organizations +1
+        self.executivePowerCache = executivePower[0]
+
+        #TODO This code just create two new powers but the never are used,
+        # and the name it's not right
+        Organization.objects.get_or_create(name='Poder Legislativo', area_id= self.argentinaCache.id,
+            defaults={
+                'classification': 'poder'
+            }
+        )
+        Organization.objects.get_or_create(name='Poder Judicial', area_id= self.argentinaCache.id,
+            defaults={
+                'classification': 'poder'
+            }
+        )
 
     def prepareCaches(self):
         self.prepareArgentinaCache()
         self.prepareExecutivePowerCache()
 
-    def createMermbership(self, date, role, organizationId, personId, postId):
-        membership = Membership(
-            created_at = date,
-            updated_at = date,
-            label = '',
-            role = '',
-            organization_id = organizationId,
-            person_id = personId,
-            post_id = postId,
-            area_id = self.argentinaCache.id,
-            start_date = '2015-12-10',
-            end_date = '2019-12-10'
-        )
-        membership.save()
-
-    def createPerson(self, date, name, lastName, mail):
-        fullName = name.decode('utf8').title() + ' ' + lastName.decode('utf8').title()
-        person = Person(
-            created_at = date,
-            updated_at = date,
-            name = fullName,
-            family_name = lastName.decode('utf8').title(),
-            given_name = name.decode('utf8').title(),
-            additional_name = '',
-            honorific_prefix = '',
-            honorific_suffix = '',
-            patronymic_name = '',
-            sort_name = '',
-            email = mail,
-            gender = '',
-            birth_date = '',
-            death_date = '',
-            summary = '',
-            biography = '',
-        )
-        person.save()
-
-        personExtra = PersonExtra(
-            base_id = person.id,
-            versions = '[]'
-        )
-        personExtra.save()
-
-        return person
-
-    def createPost(self, date, role, organizationId):
-        return Post.objects.get_or_create(
-            role = role,
-            defaults = {
-                'created_at': date,
-                'updated_at': date,
-                'label': role,
-                'organization_id': organizationId,
-                'area_id': self.argentinaCache.id
+    def createMermbership(self, role, organizationId, personId, postId):
+        membership = Membership.objects.get_or_create(person_id=personId, organization_id=organizationId, post_id= postId, area_id= self.argentinaCache.id,
+            defaults={
+                'label': '',
+                'role': role,
+                'start_date': '2015-12-10',  # time.strftime('%Y-%m-%d'),
+                'end_date': '2019-12-10'
             }
-        )[0]
-
-    def createOrganization(self, date, name, parentId):
-        organization = Organization(
-            created_at = date,
-            updated_at = date,
-            name = name,
-            classification = 'goverment',
-            parent_id = parentId,
-            description = '',
-            summary = '',
-            area_id = self.argentinaCache.id
         )
-        organization.save()
 
-        organizationExtra = OrganizationExtra(
-            register = '',
-            base_id = organization.id,
-            slug = 'goverment:' + str(organization.id)
+        if membership[1]:
+            self.memberships = self.memberships + 1
+
+    def createPerson(self, name, lastName, mail, honorPrefix):
+        lastName = lastName.decode('utf8').title()
+        name = name.decode('utf8').title()
+        fullName = name + ' ' + lastName
+        person = Person.objects.update_or_create(name=fullName, family_name=lastName, given_name=name,
+            defaults={
+                'additional_name': '',
+                'honorific_prefix': honorPrefix,
+                'honorific_suffix': '',
+                'patronymic_name': '',
+                'sort_name': '',
+                'email': mail,
+                'gender': '',
+                'birth_date': '',
+                'death_date': '',
+                'summary': '',
+                'biography': '',
+            }
         )
-        organizationExtra.save()
 
-        return organization
+        if (person[1]):
+            personExtra = PersonExtra(
+                base_id = person[0].id,
+                versions = '[]'
+            )
+            personExtra.save()
+
+            self.persons = self.persons +1
+
+        return person[0]
+
+    def createPost(self, role, organizationId):
+        post = Post.objects.get_or_create(role= role, organization_id= organizationId, area_id= self.argentinaCache.id,
+            defaults = {
+                'label': role,
+            }
+        )
+
+        if post[1]:
+            self.posts = self.posts + 1
+
+        return post[0]
+
+    def createOrganization(self, name, parentId):
+        organization = Organization.objects.get_or_create(name= name, area_id= self.argentinaCache.id,
+            defaults={
+                'classification': 'goverment',
+            }
+        )
+
+        if (organization[1]):
+            organizationExtra = OrganizationExtra(
+                register = '',
+                base_id = organization[0].id,
+                slug = 'goverment:' + str(organization[0].id)
+            )
+            organizationExtra.save()
+            self.organizations = self.organizations + 1
+
+        return organization[0]
 
     def fetchAllPositions(self):
         print ("Inserting Organizations...\n")
-        filename = 'estructura-organica.csv'
+        filename = 'estructura-organica_test.csv'
         csv_filename = join(
             dirname(__file__), '..', '..', 'data', filename
         )
         with open(csv_filename) as f:
             data = [tuple(line) for line in csv.reader(f)]
 
-        date = time.strftime('%Y-%m-%d %H:%M:%S')
         storedOrganizationsCache = {}
 
         for row in data[1:]:
@@ -147,24 +150,31 @@ class Command(BaseCommand):
             else:
                 parentId = self.executivePowerCache.id
 
-            organization = self.createOrganization(date, organizationName, parentId)
+            organization = self.createOrganization(organizationName, parentId)
             storedOrganizationsCache[organization.name.lower()] = organization.id
 
             role = row[5]
-            post = self.createPost(date, role, organization.id)
+            post = self.createPost(role, organization.id)
 
-            personName = row[7]
+            personName = row[8] #7
             if personName:
-                personLastName = row[8]
-                mail = row[20]
+                personLastName = row[7] #8
+                mail = row[19] #20
+                honorPrefix = row[6]
                 if mail:
                     mail = [x.strip() for x in mail.split(',')][0]
                 else:
                     mail = ''
-                person = self.createPerson(date, personName, personLastName, mail)
 
-                self.createMermbership(date, role, organization.id, person.id, post.id)
+                person = self.createPerson(personName, personLastName, mail, honorPrefix)
+
+                self.createMermbership(role, organization.id, person.id, post.id)
 
     def handle(self, *args, **options):
         self.prepareCaches()
         self.fetchAllPositions()
+        print 'Nuevas areas: ', self.areas
+        print 'Nuevas organizaciones: ', self.organizations
+        print 'Nuevas peronas: ', self.persons
+        print 'Nuevas puestos: ',  self.posts
+        print 'Nuevas cargos: ',  self.memberships
